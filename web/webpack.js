51e37debc5b77dec138ce4872d7d726476705c1d
Paolo Bueno <paolohaji@gmail.com>
2016-11-27 23:30:48 +0000

    Initial commit

---
+++ web/webpack.js
@@ -0,0 +1,40 @@
+const resolve = require('path').resolve;
+const webpack = require('webpack');
+const config = {
+    devServer: {
+        contentBase: resolve(__dirname, 'src')
+    },
+    entry: resolve(__dirname, '../index.web.js'),
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { cacheDirectory: true }
+            },
+            {
+                test: /\.(gif|jpe?g|png|svg)$/,
+                loader: 'url-loader',
+                query: { name: '[name].[hash:16].[ext]' }
+            }
+        ]
+    },
+    plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin()
+    ],
+    resolve: {
+        alias: {
+          'react-native': 'react-native-web'
+        }
+    }
+};
+module.exports = config;
