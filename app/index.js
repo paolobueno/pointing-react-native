4f2cc7ca1b1e36f57e2518139e224198cfb7772d
Paolo Bueno <paolohaji@gmail.com>
2016-11-29 18:55:49 -0200

    Some random content

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+var childStyle = { width: '50%', backgroundColor: '#0060aa', textAlign: 'center' }
+
 export default function () {
-  return <View>
-    <Text>Index!</Text>
-    <Text>
+  return <View style={{
+    backgroundColor: '#ddd',
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  }}>
+    <Text style={childStyle} >Index!</Text>
+    <Text style={childStyle} >
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       Mauris sagittis pellentesque lacus eleifend lacinia...
     </Text>
-    <Text>My Action</Text>
+    <Text style={childStyle} >My Action</Text>
   </View>
 }
