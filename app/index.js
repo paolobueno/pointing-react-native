0c7af0498910574803e34e7a27af554466c9f0e8
Paolo Bueno <paolohaji@gmail.com>
2016-11-29 17:48:58 -0200

    Add common code

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function () {
+  return <View>
+    <Text>Index!</Text>
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Mauris sagittis pellentesque lacus eleifend lacinia...
+    </Text>
+    <Text>My Action</Text>
+  </View>
+}
--- index.web.js
+++ index.web.js
@@ -4,47 +4,16 @@
  * @flow
  */
 
-import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './app'
 
-export default class PointingReact extends Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Web!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.web.js
-        </Text>
-        <Text>
-          test
-        </Text>
-      </View>
-    )
-  }
+export default function PointingReact () {
+  return <App />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
-
 AppRegistry.registerComponent('PointingReact', () => PointingReact)
+
 AppRegistry.runApplication('PointingReact', {
   rootTag: document.getElementById('react-root')
 })
