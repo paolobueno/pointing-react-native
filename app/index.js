5376d2e45c0af1c5f6b29a8efc5b3cf2d74d47ff
Paolo Bueno <paolohaji@gmail.com>
2016-11-30 03:17:46 +0000

    Use syncHistoryWithStore

--- app/index.js
+++ app/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { createStore, combineReducers } from 'redux'
 import { Provider, connect } from 'react-redux'
 import { Router, Route, createMemoryHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { syncHistoryWithStore, routerReducer, syncHistoryWithStore } from 'react-router-redux'
 
 import TestView from './TestView'
 
-const history = createMemoryHistory()
 const store = createStore(
   combineReducers({
     routing: routerReducer
   })
 )
+const history = syncHistoryWithStore(createMemoryHistory(), store)
 
 export default function App () {
   return (
